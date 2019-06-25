c8aaa86aa4703b51f9da0ecc2e220b2a30b499a5
Mike de Heij <mike@deheij.net>
2019-06-25 11:36:47 +0200

    Revert "Redirect to dinners when logged in"
    This reverts commit f031b1694db66bb74d6162516359a9cf9068b501.

--- src/Controller/IndexController.php
+++ src/Controller/IndexController.php
@@ -3,9 +3,7 @@
 namespace App\Controller;
 
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
-use Symfony\Component\HttpFoundation\RedirectResponse;
 use Symfony\Component\Routing\Annotation\Route;
-use Symfony\Component\Routing\RouterInterface;
 
 
 class IndexController
@@ -14,8 +12,7 @@ class IndexController
      * @Template("index.html.twig")
      * @Route("/", name="index")
      */
-    public function index(RouterInterface $router)
+    public function index()
     {
-        return new RedirectResponse($router->generate('dinner_index'), 302);
     }
 }
